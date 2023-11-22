3f773a37493fe80ea4b35770588ce3246a42f5af
Pierre Cavin <me@sherlox.io>
2023-11-22 17:38:34 +0100

    build(hex): add package metadata

--- mix.exs
+++ mix.exs
@@ -1,28 +1,42 @@
 defmodule Nodelix.MixProject do
   use Mix.Project
 
+  @version "0.0.0-alpha"
+  @source_url "https://github.com/sheerlox/nodelix"
+
   def project do
     [
       app: :nodelix,
-      version: "0.0.0-alpha",
+      version: @version,
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp description() do
+    """
+    Seamless Node.js in Elixir.
+    """
+  end
+
+  defp package() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      maintainers: ["Pierre Cavin"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
     ]
+  end
+
+  defp deps do
+    []
   end
 end
